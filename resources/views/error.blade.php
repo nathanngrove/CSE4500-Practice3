f598139037e7835f1bff54aebd4cfe5be3947ffd
Nathan Grove <nathangrove08@gmail.com>
2022-03-03 04:44:55 -0800

    testing

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -12,10 +12,11 @@
 @stop
 
 @section('css')
+    <script src="https://kit.fontawesome.com/2bff5bca0b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
     <script> console.log('Hi!'); </script>
-    <script src="https://kit.fontawesome.com/2bff5bca0b.js" crossorigin="anonymous"></script>
+    
 @stop
