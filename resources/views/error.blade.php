5526760e25c93e17b5e4421400f2353d0c24b73b
Nathan Grove <nathangrove08@gmail.com>
2022-03-03 05:01:58 -0800

    testing

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,15 +1,13 @@
 @extends('adminlte::page')
 
 @section('title', 'Error')
-<head>    
-    <script src="https://kit.fontawesome.com/2bff5bca0b.js" crossorigin="anonymous"></script>
-</head>
+
 @section('content_header')
     <h1>Error</h1>
 @stop
 
 @section('content')
-    <i class="fa-light fa-bomb"></i><h1 style="color: red;">404 Error - Page Not Found</h1>
+    <h1 style="color: red;">404 Error - Page Not Found</h1>
     <p>You can return home by clicking <a href="/">here</a></p>
 @stop
 
