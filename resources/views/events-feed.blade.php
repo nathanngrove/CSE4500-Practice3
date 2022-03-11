eb55e3ecd6497f762de967629132d5afbfd9a0e3
Nathan Grove <nathangrove08@gmail.com>
2022-03-11 12:43:49 -0800

    testing

--- resources/views/events-feed.blade.php
+++ resources/views/events-feed.blade.php
@@ -3,17 +3,17 @@
 @section('title', 'Error')
 
 @section('content')
-    echo json_encode(array(array("title" => "CSE4500 Class",
+    {{ echo json_encode(array(array("title" => "CSE4500 Class",
     "start" => "2022-02-23T17:30:00",
     "end" => "2022-02-23T18:45:00"), array("title" => "CSE4500 Class",
     "start" => "2022-02-28T17:30:00",
-    "end" => "2022-02-28T18:45:00")));
+    "end" => "2022-02-28T18:45:00"))); }}
 
     @foreach($calendarEvents AS $event)
-        $event->id
-        $event->title
-        $event->start_at
-        $event->end_at
+        {{ $event->id }}
+        {{ $event->title }}
+        {{ $event->start_at }}
+        {{ $event->end_at }}
     @endforeach
 @stop
 
