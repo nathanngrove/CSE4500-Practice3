bbb18c97314f22a1e30fc613f89afcceac2e0a84
Nathan Grove <nathangrove08@gmail.com>
2022-03-11 23:50:20 -0800

    testing

--- database/migrations/2022_03_11_194903_create_calendar_events_table.php
+++ database/migrations/2022_03_11_194903_create_calendar_events_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('calendar_events', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->date('start_at');
-            $table->date('end_at');
+            $table->DateTime('start_at');
+            $table->DateTime('end_at');
             $table->timestamps();
         });
     }
