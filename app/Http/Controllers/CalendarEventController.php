817beafe07fbb05fbb1f6ea728757fdb198006fb
Nathan Grove <nathangrove08@gmail.com>
2022-03-12 00:07:33 -0800

    testing

--- app/Http/Controllers/CalendarEventController.php
+++ app/Http/Controllers/CalendarEventController.php
@@ -15,7 +15,7 @@ class CalendarEventController extends Controller
     public function index()
     {
         $calendarEvents = CalendarEvent::all();
-        return view('events-feed', compact('calendarEvents'));
+        return view('calendar', compact('calendarEvents'));
     }
 
     /**
